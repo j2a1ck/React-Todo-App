a344d39bdb7dfc9d921445cd26c7fe5c9f96b214
jack <jack.70aw@gmail.com>
2024-06-02 18:40:17 +0330

    CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import TaskBuget from "./assets/TaskBuget"
+import TaskBudget from "./assets/TaskBudget"
+import { useState } from "react"
 
 function App() {
+  const [todo, setTodo] = useState([])
+
+  const addTodo = (newTodo) => {
+    setTodo([...todo, newTodo])
+  }
 
   return (
     <>
       <div>
-        <p className="title">Your Task</p>
-        <TaskBuget/>
+        <p className="oswald title">Your Task</p>
+        <TaskBudget addTodo={addTodo}/>
       </div>
       
     </>
---
+++ src/assets/TaskBudget.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react"
+
+export default function TaskBudget({ addTodo }){
+    const [taskText, setTaskText] = useState("");
+
+    function handleTaskChange(event){
+        setTaskText(event.target.value)
+    }
+
+    function handleButtonClick(e){
+        e.preventDefault();
+        if (taskText.trim())
+            addTodo({
+                id: Date.now(),
+                text: taskText,
+                completed: false
+            })
+            setTaskText("")
+    }
+    return(
+        <>
+        <div>
+        <div className="box">
+            <input value={taskText} onChange={handleTaskChange} placeholder="what is next? "></input>
+            <button type="submit" onClick={handleButtonClick}>add</button>
+            
+        </div>
+        </div>
+
+        </>
+    )
+}
