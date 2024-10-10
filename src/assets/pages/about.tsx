542bdd88ea642225861a9ddc08e0c4e3827052c0
Jack <jack.70aw@gmail.com>
2024-10-10 15:53:27 +0330

    Update about

--- src/assets/pages/about.tsx
+++ src/assets/pages/about.tsx
@@ -2,19 +2,17 @@ import React from "react";
 
 const About: React.FC = () => {
   return (
-    <>
     <div style={{ color: "white", margin: "50px", textAlign: "center" }}>
       <h1 style={{color: "pink"}}>
         About Page
       </h1>
       <p> this is very simple todo app made with React </p>
       <p>
-        you can visit code in  
-        <a href="https://github.com/j2a1ck/React-Todo-App"> github</a>
+        you can visit code in
+        <a href="https://github.com/j2a1ck/React-Todo-App">github</a>
       </p>
       <p> made by Me with ❤</p>
     </div>
-    </>
   );
 };
 export default About;
